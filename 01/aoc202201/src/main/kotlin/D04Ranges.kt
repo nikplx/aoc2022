47f2a398d7e130870474ba4fa1e64bd081ae76be
Niklas Paulicks <paulicks@justtrack.io>
2022-12-05 09:34:54 +0100

    day 4

---
+++ 01/aoc202201/src/main/kotlin/D04Ranges.kt
@@ -0,0 +1,21 @@
+object D04Ranges {
+    fun getFullyContainedPairs(inp: String): Int {
+        return inp.split("\n")
+            .map { it.split(",") }
+            .map { scoreFromRange(it) }
+            .reduce { acc, i -> acc + i}
+    }
+
+    private fun scoreFromRange(r: List<String>): Int {
+        val (r1, r2) = r.map { s ->
+            val splitted = s.split("-").map(String::toInt)
+            (splitted[0]..splitted[1])
+        }
+
+        if (r1.intersect(r2).size == r1.toList().size || r2.intersect(r1).size == r2.toList().size) {
+            return 1
+        }
+
+        return 0
+    }
+}
